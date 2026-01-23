98467d1d336cf54bdf0c9132718ce28e15c9ce12
Justin Morgan <2justinmorgan@gmail.com>
2026-01-23 02:34:33 -0800

    Type py files via mypy

--- src/common.py
+++ src/common.py
@@ -1,10 +1,11 @@
+from typing import Union as _Union
 from os import (
     getenv as _getenv,
     environ as _environ,
 )
 
 
-def get_env(var_name: str) -> str:
+def get_env(var_name: str) -> _Union[str, None]:
     if var_name not in _environ:
         raise KeyError(f"env-var '{var_name}' not found")
     return _getenv(var_name)
--- src/liquid.py
+++ src/liquid.py
@@ -1,6 +1,9 @@
 from typing import (
-    Optional as _Optional,
+	cast as _cast,
+	Optional as _Optional,
 	List as _List,
+	Any as _Any,
+	Dict as _Dict,
 )
 from requests import request as _request
 from http import HTTPMethod as _HTTPMethod
@@ -10,9 +13,9 @@ from common import get_env as _get_env
 def _query(
 	method: _HTTPMethod,
 	api_url_path: str,
-	data: _Optional[dict] = None,
+	data: _Optional[_Dict[str, _Any]] = None,
 	session_token: _Optional[str] = None,
-) -> dict:
+) -> _Any:
 	base_url = _get_env("LIQUID_API_BASE_URL")
 	url = f"{base_url}{'/' if api_url_path[0] != '/' else ''}{api_url_path}"
 	return _request(
@@ -27,7 +30,8 @@ def _query(
 
 
 def _get_session_token() -> str:
-	return _query(
+	tkey = "sessionToken"
+	result = _query(
 		_HTTPMethod.POST,
 		"/dxsca-web/login",
 		{
@@ -35,13 +39,19 @@ def _get_session_token() -> str:
 			"password": _get_env("LIQUID_PW"),
 			"domain": "default",
 		},
-	).get("sessionToken")
+	)
+	if not isinstance(result, dict) or not isinstance(result.get(tkey), str):
+		raise TypeError(f"session token not received")
+	return _cast(str, result.get(tkey))
 
 
-def get_instruments() -> _List:
-	return _query(
+def get_instruments() -> _List[_Dict[str, _Any]]:
+	result = _query(
 		_HTTPMethod.GET,
 		"dxsca-web/instruments/query",
 		None,
 		_get_session_token(),
 	)
+	if not isinstance(result, list):
+		raise TypeError(f"instruments not received")
+	return result
